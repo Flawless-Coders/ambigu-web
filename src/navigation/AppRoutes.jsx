5a435ae8f3eb6026d815594a362345511e890744
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2025-03-15 22:23:19 -0600

    [UPDATE] layout and navbar - handle show navbar

--- src/navigation/AppRoutes.jsx
+++ src/navigation/AppRoutes.jsx
@@ -9,6 +9,7 @@ import ForgotPasswordPage from "../modules/auth/pages/ForgotPasswordPage"
 import ProfilePage from "../modules/profile/pages/ProfilePage"
 import LoaderAmbigu from "../kernel/LoaderAmbigu"
 import WaitersPage from "../modules/waiters/pages/WaitersPage"
+import { ThemesPage } from "../modules/theming/pages/ThemesPage"
 
 // Protected route component
 const ProtectedRoute = ({ children }) => {
@@ -68,7 +69,7 @@ const AppRoutes = () => {
         <Route path="/dishes" element={<div>Platillos (Coming Soon)</div>} />
         <Route path="/tables" element={<div>Mesas (Coming Soon)</div>} />
         <Route path="/menu" element={<div>Menú (Coming Soon)</div>} />
-        <Route path="/customization" element={<div>Personalización (Coming Soon)</div>} />
+        <Route path="/customization" element={<ThemesPage />} />
         <Route path="/orders" element={<div>Pedidos (Coming Soon)</div>} />
         <Route path="/profile" element={<ProfilePage/>} />
       </Route>
