f16aeca23f4d32583a60e2e1289e65d139bebb34
JoseLuisMirRo <169010668+JoseLuisMirRo@users.noreply.github.com>
2025-03-21 23:47:25 -0600

    [UPDATE] Typography management

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,7 +1,19 @@
-import { createContext, useContext, useMemo } from "react";
+import { createContext, useContext, useMemo, useEffect } from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { AuthContext } from "./AuthContext";
 import baseTheme from "../config/theme";
+
+const loadFontInDOM = (font) => {
+  const fontId = `font-${font.replace(/ /g, "-")}`;
+  if (!document.getElementById(fontId)) {
+    const link = document.createElement("link");
+    link.id = fontId;
+    link.href = `https://fonts.googleapis.com/css?family=${font.replace(/ /g, "+")}`;
+    link.rel = "stylesheet";
+    document.head.appendChild(link);
+  }
+};
+
 
 const normalizeTheme = (themeData) => {
   return createTheme({
@@ -18,7 +30,7 @@ const normalizeTheme = (themeData) => {
       },
       sidebar: {
         bg: themeData?.sidebarBg || baseTheme.palette.sidebar.bg,
-        text: themeData?.sidebarText || baseTheme.palette.sidebar.text,
+        text: baseTheme.palette.sidebar.text,
         hover: themeData?.sidebarHover || baseTheme.palette.sidebar.hover,
         selected: themeData?.sidebarSelected || baseTheme.palette.sidebar.selected,
       },
@@ -29,6 +41,27 @@ const normalizeTheme = (themeData) => {
     },
     typography: {
       fontFamily: themeData?.bodyFont || baseTheme.typography.fontFamily,
+      h1: {
+        fontFamily: themeData?.headerFont || baseTheme.typography.h1.fontFamily,
+      },
+      h2: {
+        fontFamily: themeData?.headerFont || baseTheme.typography.h2.fontFamily,
+      },
+      h3: {
+        fontFamily: themeData?.headerFont || baseTheme.typography.h3.fontFamily,
+      },
+      body1: {
+        fontFamily: themeData?.bodyFont || baseTheme.typography.body1.fontFamily,
+      },
+      body2: {
+        fontFamily: themeData?.paragraphFont || baseTheme.typography.body2.fontFamily,
+      },
+      logo: {
+        fontFamily: themeData?.logoFont || baseTheme.typography.logo.fontFamily,
+      },
+      footer: {
+        fontFamily: themeData?.footerFont || baseTheme.typography.footer.fontFamily,
+      }
     },
   });
 };
@@ -37,6 +70,13 @@ export const ThemeContext = createContext();
 
 export const ThemeProviderComponent = ({ children }) => {
   const { themeData } = useContext(AuthContext);
+
+  // Cargar la fuente dinámica cuando themeData.bodyFont cambie
+  useEffect(() => {
+    if (themeData?.bodyFont) {
+      loadFontInDOM(themeData.bodyFont);
+    }
+  }, [themeData?.bodyFont]);
 
   // Normalizar y aplicar el tema
   const theme = useMemo(() => normalizeTheme(themeData), [themeData]);
